1d60d9d72ca82db83b6ccb33baa43046caeeec3c
KD <kittoo007@gmail.com>
2025-05-25 15:43:38 +0200

    access data from json file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,5 +1,28 @@
+import json
+
+def load_data(file_name):
+    with open(file_name, 'r') as fileobj :
+        return json.load(fileobj)
+
+
+
 def main():
     print("Hello from zootopia!")
+    animals_data = load_data('animals_data.json')
+    print(animals_data)
+    for animal in animals_data:
+        print(f"Name: {animal['name']}")
+
+        if 'diet' in animal['characteristics']:
+            print(f"Diet: {animal['characteristics']['diet']}")
+
+        if 'locations' in animal and animal['locations']:
+            print(f"Location: {animal['locations'][0]}")
+
+        if 'type' in animal['characteristics']:
+            print(f"Type: {animal['characteristics']['type']}")
+
+        print()
 
 
 if __name__ == "__main__":
